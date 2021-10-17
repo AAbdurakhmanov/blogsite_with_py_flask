2db0227ca33017e2b90eda4786b646a303761ac5
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2021-10-17 16:00:58 +0500

    change index blog post

--- app.py
+++ app.py
@@ -1,25 +1,4 @@
-import datetime
-import functools
-import os
-import re
-import urllib
-from flask import (Flask, abort, flash, Markup, redirect, render_template, request, Response, session, url_for)
-from flask_sqlalchemy import SQLAlchemy
-from flask.templating import render_template
-from werkzeug.exceptions import abort
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
-
-class Blogpost(db.model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(50))
-  subtitle = db.Column(db.String(50))
-  author = db.Column(db.String(20))
-  date_posted = db.Column(db.DateTime)
-  content = db.Column(db.Text)
-
+from models import *
 
 
 # Route part
@@ -29,7 +8,8 @@ def index():
 
 @app.route('/blog')
 def blog():
-  return render_template('blog.html')
+  posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+  return render_template('blog.html', posts=posts)
 
 @app.route('/about')
 def about():
@@ -40,47 +20,10 @@ def some():
   return render_template('some.html')
 
 
-# # Models part
-# class Entry(db.Model):
-#     title = CharField()
-#     slug = CharField(unique=True)
-#     content = TextField()
-#     published = BooleanField(index=True)
-#     timestamp = DateTimeField(default=datetime.datetime.now, index=True)
-
-#     def save(self, *args, **kwargs):
-#         if not self.slug:
-#             self.slug = re.sub('[^\w]+', '-', self.title.lower())
-#         ret = super(Entry, self).save(*args, **kwargs)
-
-#         # Store search content.
-#         self.update_search_index()
-#         return ret
-
-#     def update_search_index(self):
-#         search_content = '\n'.join((self.title, self.content))
-#         try:
-#             fts_entry = FTSEntry.get(FTSEntry.docid == self.id)
-#         except FTSEntry.DoesNotExist:
-#             FTSEntry.create(docid=self.id, content=search_content)
-#         else:
-#             fts_entry.content = search_content
-#             fts_entry.save()
-
-
-# class FTSEntry(FTSModel):
-#     content = SearchField()
-
-#     class Meta:
-#         database = database
-
-
-# # Forms part
-
-
-
-
-
+@app.route('/post/<int:post_id>')
+def post(post_id):
+  post = Blogpost.query.filter_by(id=post_id).one()
+  return render_template('blog.html', post=post)
 
 if __name__ == '__main__':
   app.run(debug=True, port=5001)
---
+++ models.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from flask import (Flask, redirect, render_template, request, Response, session, url_for)
+from flask_sqlalchemy import SQLAlchemy
+from flask.templating import render_template
+from werkzeug.exceptions import abort
+from flask_migrate import Migrate, migrate
+from flask_share import Share
+from flask_user import UserManager, roles_accepted, roles_required, user_registered, login_required
+
+app = Flask(__name__)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+app.config['SECURITY_POST_LOGIN'] = '/profile'
+app.config['SOCIAL_FACEBOOK'] = {
+    'consumer_key': 'facebook app id',
+    'consumer_secret': 'facebook app secret'
+}
+
+
+
+db = SQLAlchemy(app)
+share = Share(app)
+migrate = Migrate(app, db)
+
+
+
+
+class Blogpost(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(50))
+  subtitle = db.Column(db.String(50))
+  date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+  content = db.Column(db.Text)
+  
